5d6c17724319349a09b1a517aef9c4f19415794e
DM Tech <odeydivine16@gmail.com>
2026-06-29 19:10:52 +0100

    Started practising javascript

---
+++ Javascript practise/intro.js
@@ -0,0 +1,11 @@
+// variables in js
+var name = "divine";
+var age = 25;
+var nigerian = true;
+var empty = null;
+var undef = undefined
+console.log(name)
+console.log(age)
+console.log(nigerian)
+console.log(empty)
+console.log(undef);
